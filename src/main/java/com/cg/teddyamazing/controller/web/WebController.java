5ff378c3bb3adfdf945d7f432ddc3648e929a43c
thucuc01 <70868459+thucuc01@users.noreply.github.com>
2021-02-25 11:33:02 +0700

    web

--- src/main/java/com/cg/teddyamazing/controller/web/WebController.java
+++ src/main/java/com/cg/teddyamazing/controller/web/WebController.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/web")
 public class WebController {
     @Autowired
     public ManagerProductService managerProductService;
